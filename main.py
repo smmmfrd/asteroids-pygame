2a0c4d17967180635cdcb979bc10520289fec9d9
Sam Mumford <smmmfrd@gmail.com>
2025-10-14 16:32:34 -0700

    Implement clock and delta time

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -15,6 +18,7 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
